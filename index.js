dd05089acc08ba1f8c822f58cd25f3c0d67009a3
aya@osx <ravinderssahni@gmail.com>
2019-10-08 20:23:06 -0400

    remove unnecessary declarations

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 
 const env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
 	require('dotenv').config();
 }
 
-const authRouther = require('./routes/auth.router');
-
-app.use('/auth', authRouther);
+app.use('/auth', require('./routes/auth.router'));
 app.listen(process.env.PORT);
