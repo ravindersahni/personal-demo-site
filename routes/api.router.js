a83d65d1d8d5f7e1fbe05731f6b1d4ad468ddd56
aya@osx <ravinderssahni@gmail.com>
2019-10-12 17:11:42 -0400

    redirect to landing on log out

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,6 +3,6 @@ const apiRouter = require('express').Router();
 module.exports = apiRouter
 	.get('/logout', (req, res) => {
 		req.logout();
-		res.redirect('/auth/google');
+		res.redirect('/');
 	})
 	.get('/current-user', (req, res) => res.send(req.user));
