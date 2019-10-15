7b13388525aebdffb0bdeb1af41ff5cdbe6b4602
aya@osx <ravinderssahni@gmail.com>
2019-10-15 00:12:36 -0400

    lowercase app.component

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/App.component';
+import App from './components/app.component';
 
 ReactDOM.render(
 	<Provider store={store}>
