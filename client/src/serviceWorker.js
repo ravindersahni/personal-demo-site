ac169cadff03e9b234c6fc936e11399023e459d3
aya@osx <ravinderssahni@gmail.com>
2019-10-15 16:43:59 -0400

    configure serviceWorker

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -33,16 +33,16 @@ export function register(config) {
 
 		window.addEventListener('load', () => {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-			function isAuthRoute() {
-				return window.location.pathname.startsWith('/auth');
-			}
-			if (isAuthRoute()) {
-				console.info('unregistering service worker for auth route');
-				unregister();
-				console.info('reloading');
-				window.location.reload();
-				return false;
-			}
+			// function isAuthRoute() {
+			// 	return window.location.pathname.startsWith('/auth');
+			// }
+			// if (isAuthRoute()) {
+			// 	console.info('unregistering service worker for auth route');
+			// 	unregister();
+			// 	console.info('reloading');
+			// 	window.location.reload();
+			// 	return false;
+			// }
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
