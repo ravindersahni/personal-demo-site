a7a94ec91d3384cb5b6d31d43bd2c30a9f0f742e
aya@osx <ravinderssahni@gmail.com>
2019-10-15 00:00:43 -0400

    capitalize App.component

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/app.component';
+import App from './components/App.component';
 
 ReactDOM.render(
 	<Provider store={store}>
