25685e23c60f404c268e78d0ded3b4e08e1e8a3f
aya@osx <ravinderssahni@gmail.com>
2019-10-15 17:39:09 -0400

    disable serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/app.component';
-// import * as ServiceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -14,5 +13,3 @@ ReactDOM.render(
 	</Provider>,
 	document.querySelector('#root')
 );
-
-// ServiceWorker.register();
