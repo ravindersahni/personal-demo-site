89cdcdd81966b99f7ebda3284bc212bfb0857d2b
aya@osx <ravinderssahni@gmail.com>
2019-10-15 22:34:23 -0400

    create and apply styled EnsoArticle

---
+++ client/src/components/enso-article/enso-article.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Enso } from '../../assets/images/enso.svg';
+
+const StyledArticle = styled.article`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+`;
+
+const StyledEnso = styled(Enso)`
+	max-width: 60%;
+	@media(min-width: 800px) {
+		max-width: 36%;
+	}
+`;
+
+const EnsoArticle = ({ children }) => (
+	<StyledArticle>
+		<StyledEnso />
+		{children}
+	</StyledArticle>
+);
+
+export default EnsoArticle;
--- client/src/pages/dashboard/dashboard.component.jsx
+++ client/src/pages/dashboard/dashboard.component.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { ReactComponent as Enso } from '../../assets/images/enso.svg';
+import EnsoArticle from '../../components/enso-article/enso-article.component';
 
-const Dashboard = () => {
-	return (
-		<article
-			style={{
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				height: '100%'
-			}}
-		>
-			<Enso style={{ maxWidth: '36%' }} />
-		</article>
-	);
-};
+const Dashboard = () => <EnsoArticle />;
 
 export default Dashboard;
