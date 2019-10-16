65dc8cfdbf081a6b4e2610a3511ebdec186ab8f2
aya@osx <ravinderssahni@gmail.com>
2019-10-15 21:35:40 -0400

    replace hidden text with aria-label

--- client/src/components/github-logo-link/github-logo-link.component.jsx
+++ client/src/components/github-logo-link/github-logo-link.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ReactComponent as GithubLogo } from '../../assets/images/github.svg';
 
 const GithubLogoLink = () => (
-	<a href="https://github.com/ravindersahni/personal-demo-site">
-		<span style={{ visibility: 'hidden' }}>Github</span>
+	<a
+		href="https://github.com/ravindersahni/personal-demo-site"
+		aria-label="Link to Github source of this site"
+	>
 		<GithubLogo style={{ verticalAlign: 'middle' }} />
 	</a>
 );
