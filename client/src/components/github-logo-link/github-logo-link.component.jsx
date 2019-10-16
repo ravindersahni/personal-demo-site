a5fdad091874f8de89bbd484c0915dcd0ef0f60a
aya@osx <ravinderssahni@gmail.com>
2019-10-15 21:23:02 -0400

    add link text for accessibility

--- client/src/components/github-logo-link/github-logo-link.component.jsx
+++ client/src/components/github-logo-link/github-logo-link.component.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as GithubLogo } from '../../assets/images/github.svg';
 
 const GithubLogoLink = () => (
 	<a href="https://github.com/ravindersahni/personal-demo-site">
+		<span style={{ visibility: 'hidden' }}>Github</span>
 		<GithubLogo style={{ verticalAlign: 'middle' }} />
 	</a>
 );
