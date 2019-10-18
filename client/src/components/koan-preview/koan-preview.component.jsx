867dd7a811a0d51fc58b8272d697c36c07c1b19c
aya@osx <ravinderssahni@gmail.com>
2019-10-18 14:23:40 -0400

    move styles to separate file

--- client/src/components/koan-preview/koan-preview.component.jsx
+++ client/src/components/koan-preview/koan-preview.component.jsx
@@ -1,59 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { ReactComponent as LockIcon } from '../../assets/images/lock.svg';
-
-const StyledArticle = styled.article`
-	display: flex;
-	justify-content: center;
-	width: 100%;
-	border-radius: 100%;
-	padding: 0;
-	background-color: #dedede;
-	color: #232323;
-	transition: background-color 0.5s, color 0.5s, font-size 0.5s, letter-spacing 0.5s;
-	cursor: pointer;
-
-	:hover {
-		background-color: ${props => (props.locked ? 'firebrick' : 'darkslategrey')};
-		color: ghostwhite;
-		font-size: 1.15rem;
-		letter-spacing: -1px;
-		svg {
-			fill: lightgrey;
-		}
-	}
-
-	::before {
-		content: '';
-		display: block;
-		padding-top: 100%;
-	}
-`;
-
-const StyledH1 = styled.h1`
-	width: 100%;
-	max-width: 300px;
-	margin: 0;
-	padding: 1rem;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-`;
-
-const LockedH1 = styled(StyledH1)`
-	margin-bottom: 1.5rem;
-`;
-
-const FlexLink = styled(Link)`display: flex; width: 100%`;
-
-const KoanLockIcon = styled(LockIcon)`
-	height: 1.5rem;
-	fill: orangered;
-	transition: fill 1s;
-`;
+import {
+	StyledArticle,
+	StyledH1,
+	LockedH1,
+	FlexLink,
+	KoanLockIcon
+} from './koan-preview.styles.jsx';
 
 const LockedHeader = ({ children }) => (
 	<LockedH1>
